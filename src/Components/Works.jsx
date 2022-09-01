0a8aa2c39f3d486b903ac437c1a2d72f63fd6c27
Inbasekar <inbatech2024@gmail.com>
2022-09-01 22:55:41 +0530

    ngtm added

--- src/Components/Works.jsx
+++ src/Components/Works.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Card from "./Card";
 import Sdata from "./Sdata";
-
+import govt from "../assests/appicons/govt.png";
 
 const Works = () =>{
     return (
@@ -10,6 +10,18 @@ const Works = () =>{
         <div className="my-5">
                 <h1 className="text-center" style={{fontWeight:"bolder",fontFamily:"Merriweather",fontSize:"3rem"}}>Works</h1>
             </div>      
+            <div className="card-container">
+                <div className="image-container">
+                <a href="http://www.ngtmboard.com" className="ngtm"><img src={govt} alt='logo'/></a>
+                </div>
+                <div className="card-title">
+               <h1>NGTM</h1>
+                    <h4>(OnGoing)</h4>
+                </div>
+                <div className="card-body">
+                    <h5>Plastic Free Tamil Nadu is a movement which is a larger than ever approach to curb plastic and related products’ pollution from the state. It envisions to create a large network of school students who will act as young ambassadors for the movement. This approach of inculcating sustainability in our state from the grass-root level would bear fruits in the years to come.</h5>
+                </div>
+            </div>
             <div className="container-fluid mb-5">
                 <div className="row">
                     <div className="col-10 mx-auto">
